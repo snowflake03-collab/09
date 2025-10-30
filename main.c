a3e0aa8dda2395cb7729a4c4de6acba0d1d59645
snowflake03-collab <snowflake03@sookmyung.ac.kr>
2025-10-30 16:04:56 +0900

    09-3

--- main.c
+++ main.c
@@ -4,26 +4,51 @@
 
 int main(void)
 { 
-  int i, average;
-  int sum;
+#if 0
+  int i;
   int grade[SIZE];
-  printf("%d명의 점수를 입력하세요.\n", SIZE); 
-  
-  sum = 0;  
+  int score[SIZE];
+   
   for(i=0; i<SIZE; i++){
-           scanf("%d", &grade[i]);
-           sum += grade[i];
-  }
-  
-  average = sum / SIZE;
-  printf("average grade: %d\n", average);
-  
-  //#if 0 -> 주석 처리랑 똑같은 효과, 지우기엔 아까울때 
-  #if 0 
-  for( i=0; i<SIZE; i++)
-       printf(“grade[%d] = %d\n”, i, grade[i]);
- #endif
-  
+           grade[i] = rand() % 100;
+           score[i] = grade[i];
+}
+  for(i=0; i<SIZE; i++){
+           printf("score[%d] = %d\n", i, score[i]);
+           printf("grade[%d] = %d\n", i, grade[i]);
+}
+#endif
+ 
+ int i;
+ 
+ int a[SIZE] = {1, 2, 3, 4, 5};
+ int b[SIZE] = {1, 2, 3, 4, 5};
+ int flag_same = 1;
+
+ for(i=0; i<SIZE; i++)
+ {
+          #if 0
+          if (a[i] == b[i])
+             printf("index %i: same\n", i);
+          else
+              printf("index %i: different\n", i); 
+          #endif
+          
+          if (a[i] != b[i])
+             printf("%dth element is not same\n", i); 
+             flag_same = 0;
+}          
+printf("Are a and b the same? %i\n", flag_same); 
+
+               
+#if 0
+ if (a==b) //array가 시작되는 메모리 번지수 
+    printf(“배열은 같은 값을 가집니다.\n”);
+ else               
+    printf(“배열은 다른 값을 가집니다.\n”)
+
+#endif
+
   system("PAUSE");	
   return 0;
 }
